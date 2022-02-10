e20421c603644a9fd16db3bf5466039580b05951
Rohit Deo <56561647+rodeo22@users.noreply.github.com>
2022-02-10 09:58:27 +0530

    Add files via upload

---
+++ EmailExchangeService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Exchange.WebServices.Data;
+using System.Configuration;
+
+namespace ExcahnegEmailAccess
+{
+    class EmailExchangeService
+    {
+        //To read email message by subject
+        public static EmailMessage VerifyEmailNotification(string subject)
+        {
+            var service = GetEmailExchangeService();
+            foreach (Item email in service.FindItems(WellKnownFolderName.Inbox, new ItemView(5)))
+            {
+                EmailMessage message = EmailMessage.Bind(service, email.Id);
+                if (message.Subject.Contains(subject) && message.DateTimeReceived >= DateTime.Now.AddMinutes(-5))
+                {
+                    return message;
+                }
+            }
+            //Check in JunkEmail if not found in Inbox.
+            foreach (Item email in service.FindItems(WellKnownFolderName.JunkEmail, new ItemView(5)))
+            {
+                EmailMessage message = EmailMessage.Bind(service, email.Id);
+                if (message.Subject.Contains(subject) && message.DateTimeReceived >= DateTime.Now.AddMinutes(-5))
+                {
+                    return message;
+                }
+            }
+            return null;
+        }
+
+        //To get exchangeService object
+        private static ExchangeService GetEmailExchangeService()
+        {
+            string userName = ConfigurationManager.AppSettings["UserName"].ToString();
+            string password = ConfigurationManager.AppSettings["Password"].ToString();
+            ExchangeService service = null;
+            try
+            {
+                service = new ExchangeService { Credentials = new WebCredentials(userName, password) };
+                service.Url = new Uri(ConfigurationManager.AppSettings["OutlookExchangeURL"]);
+            }
+            catch
+            {
+                Console.WriteLine("Email exchange connection/login failed");
+            }
+            return service;
+        }
+    }
+}
